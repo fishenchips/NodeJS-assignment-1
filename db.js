7afd0019a8d4c1d80e5032e6929f4fad9d3817ac
Philip Carlsson <philipcarlsson@hotmail.com>
2022-04-05 13:56:28 +0200

    create db cars

--- db.js
+++ db.js
@@ -3,6 +3,8 @@ const sqlite = require("sqlite3");
 
 const db = new sqlite.Database("database.db");
 
+/* ------------------------------ accounts ------------------------------------ */
+
 //create db-table if it doesnt exist
 db.run(`
     Create Table if not Exists accounts (
@@ -51,4 +53,48 @@ exports.getAllAccounts = function (callback) {
       Select * from accounts
     `;
   db.all(query, callback);
+};
+
+/* -----------------------CARS----------------------------*/
+
+//create new table for cars if it doesnt exist
+db.run(`
+      Create Table if Not Exists cars (
+        id Integer Primary Key,
+        make Text,
+        model Text
+      )
+`);
+
+//save car in db
+module.exports.registerCar = (make, model, callback) => {
+  const query = `
+    Insert Into cars
+      (make, model)
+    Values
+      (?, ?)
+  `;
+
+  const values = [make, model];
+
+  db.run(query, values, callback);
+};
+
+//get a car from db
+exports.getCarById = function (id, callback) {
+  const query = `
+    Select * from cars where id = ?
+  `;
+  const values = [id];
+
+  db.get(query, values, callback);
+};
+
+//get a list of all cars
+exports.getAllCars = function (callback) {
+  const query = `
+    Select * from cars
+  `;
+
+  db.all(query, callback); //all instead of get to see the entire array
 };
